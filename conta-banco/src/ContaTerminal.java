6e4ba22c390f72b1aee648442491b977bc54bfad
= <=>
2023-03-01 18:59:23 -0300

    banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,6 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, mudo World!");
-        System.out.println("d 222");
+        System.out.println("d 2");
     }
 }
